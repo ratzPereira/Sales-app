813b3a1c091057f6fb25affad6fb2dab714878f2
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-10-30 13:57:22 +0000

    created endpoint to update user by id

--- src/main/java/com/ratz/controller/ClientController.java
+++ src/main/java/com/ratz/controller/ClientController.java
@@ -20,33 +20,45 @@ public class ClientController {
     @ResponseBody
     public ResponseEntity getClientById(@PathVariable Integer id) {
 
-       Optional<Client> client = clientService.findClientById(id);
+        Optional<Client> client = clientService.findClientById(id);
 
-       if(!client.isPresent()){
-           System.out.println("Not Found");
-           return ResponseEntity.notFound().build();
+        if (!client.isPresent()) {
+            System.out.println("Not Found");
+            return ResponseEntity.notFound().build();
 
-       }
-       System.out.println("Found");
-       return ResponseEntity.ok(client);
+        }
+        System.out.println("Found");
+        return ResponseEntity.ok(client);
     }
 
     @PostMapping()
     @ResponseBody
-    public ResponseEntity saveClient(@RequestBody Client client){
+    public ResponseEntity saveClient(@RequestBody Client client) {
         Client clientSaved = clientService.saveClient(client);
         return ResponseEntity.ok(clientSaved);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteUserById(@PathVariable Integer id){
+    public ResponseEntity deleteUserById(@PathVariable Integer id) {
 
         Optional<Client> clientToDelete = clientService.findClientById(id);
 
-        if(clientToDelete.isPresent()) {
+        if (clientToDelete.isPresent()) {
             clientService.deleteClientById(id);
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
     }
+
+    @PutMapping("{id}")
+    public ResponseEntity updateUserById(@PathVariable Integer id, @RequestBody Client client) {
+
+        return clientService.findClientById(id).map(actualClient -> {
+            client.setId(actualClient.getId());
+            clientService.saveClient(client);
+            return ResponseEntity.noContent().build();
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
+
+
