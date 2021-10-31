da274c23e7f6760756e181576cd8d249ceb694b6
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-10-31 19:17:57 -0100

    Added all endpoints to Product Controller and tested in postman

--- src/main/java/com/ratz/controller/ProductController.java
+++ src/main/java/com/ratz/controller/ProductController.java
@@ -2,9 +2,13 @@ package com.ratz.controller;
 
 import com.ratz.entity.Product;
 import com.ratz.service.ProductServiceImpl;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -18,14 +22,44 @@ public class ProductController {
 
 
     @GetMapping("/{id}")
-    public Product getProductById(@PathVariable Integer id){
+    public Product getProductById(@PathVariable Integer id) {
         return this.productService.findProductById(id).orElseThrow(() ->
                 new ResponseStatusException(HttpStatus.NOT_FOUND, "Product with the id " + id + " not found."));
     }
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    public Product saveProduct(@RequestBody Product product){
+    public Product saveProduct(@RequestBody Product product) {
         return productService.saveProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProductById(@PathVariable Integer id) {
+        productService.findProductById(id).map(product -> {
+            productService.deleteProductById(id);
+            return product;
+        }).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Product with the id " + id + " not found."));
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void updateProductById(@PathVariable Integer id,@RequestBody Product product){
+
+        productService.findProductById(id).map(productUpdated -> {
+            product.setId(productUpdated.getId());
+            productService.saveProduct(product);
+            return product;
+        }).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Product with the id " + id + " not found."));
+    }
+
+    @GetMapping("/find")
+    public List<Product> findProducts (Product filter){
+
+        ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreCase().withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+        Example example = Example.of(filter,matcher);
+        return productService.findAllProducts(example);
     }
 }
