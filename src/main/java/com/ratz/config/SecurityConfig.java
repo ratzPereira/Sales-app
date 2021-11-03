f6018dd9b1586514fc69056efb573dfc1a366939
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-11-03 16:29:56 -0100

    added spring security to the project, created class securityConfig and started the app security configuration

---
+++ src/main/java/com/ratz/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.ratz.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("Admin")
+                .password(passwordEncoder()
+                .encode("12345"))
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/client/**").authenticated()
+                .and().formLogin();
+    }
+}
